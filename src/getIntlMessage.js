c8e4761c0d9cefccca47cbf16d6673270174319b
Dave Batiste <dave.batiste@d2l.com>
2015-08-14 15:08:40 -0400

    Adding reference to getIntlMessage for source of original logic.

--- src/getIntlMessage.js
+++ src/getIntlMessage.js
@@ -1,4 +1,10 @@
 
+/*
+The code is copied from react-intl mixin with slight modification
+to accept messages as a parameter so that it does not rely
+directly on React component properties or context.
+See: https://github.com/yahoo/react-intl
+*/
 export default (path, messages) => {
 
 	let pathParts = path.split('.');
